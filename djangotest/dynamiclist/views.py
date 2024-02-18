5692614f03ace51f475613cfa350403be6ac8174
Ved J0shi <vedjoshi19104@gmail.com>
2024-02-18 18:56:37 -0500

    updated views indynamiclist

--- djangotest/dynamiclist/views.py
+++ djangotest/dynamiclist/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-tasks = ["foo", "bar", "baz"] #dynamic list
+tasks = [] #dynamic list, global variable
 # Create your views here.
 def index(request):
     return render(request, "todo/index.html", {"tasks": tasks})
 
-def add(request):
+def add(request): #add is called either when going to url (GET) OR when submitting to the form (POST)
+    if request.method == 'POST':
+        task = request.POST["task"] #POST attribute contains POST variables as a dict
+        tasks.append(task)  
     return render(request, "todo/form.html", {} )
