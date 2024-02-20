3dc278d1931467188a88e02210a0006efc63e494
Ved J0shi <vedjoshi19104@gmail.com>
2024-02-20 16:53:58 -0500

    updated blog app in website

--- djangoproj/website/blog/urls.py
+++ djangoproj/website/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
+    path('', views.home, name="blog-home"), #3rd arg is name of the url route
     path('about/', views.about, name="blog-about")
 ]
--- djangoproj/website/blog/views.py
+++ djangoproj/website/blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+posts =[
+    {
+        'author': 'Vedang',
+        'title': 'First Post',
+        'content': 'first post content...'
+    },
+    {
+        'author': 'Spock',
+        'title': 'Second Post',
+        'content': 'second post content...'
+    } ] #will be passed into a template as context variable
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Homepage</h1>')
+    #return HttpResponse('<h1>Blog Homepage</h1>')
+    return render(request, 'blog/home.html', {'posts': posts}) #render still returns an HTTP Response
 
 def about(request):
-    return HttpResponse('<h1>About this Blog</h1>')
+    #return HttpResponse('<h1>About this Blog</h1>')
+    return render(request, 'blog/about.html')
