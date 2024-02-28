84bd3775369446f621b58420c80291bb4839f301
Ved J0shi <vedjoshi19104@gmail.com>
2024-02-28 16:26:49 -0500

    updated dynamiclist in djangotest

--- djangotest/dynamiclist/views.py
+++ djangotest/dynamiclist/views.py
@@ -6,7 +6,8 @@ def index(request):
     return render(request, "todo/index.html", {"tasks": tasks})
 
 def add(request): #add is called either when going to url (GET) OR when submitting to the form (POST)
+    task = None
     if request.method == 'POST':
         task = request.POST["task"] #POST attribute contains POST variables as a dict
         tasks.append(task)  
-    return render(request, "todo/form.html", {} )
+    return render(request, "todo/form.html", {"newtask":task} )
