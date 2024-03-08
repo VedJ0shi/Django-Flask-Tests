c6622abf411178fd2e22dda2c8ea03d5939e34bc
Ved J0shi <vedjoshi19104@gmail.com>
2024-03-08 17:43:49 -0500

    integrated Blogpost model into blog app

--- djangoproj/website/blog/models.py
+++ djangoproj/website/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blogpost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "posts") #User is the related table (model class)
+    #author will be an object of the User model class
+
+    def __str__(self):
+        return f"{self.title}|{len(self.content)} chars|by {self.author}"
--- djangoproj/website/blog/views.py
+++ djangoproj/website/blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-
-posts =[
-    {
-        'author': 'Vedang',
-        'title': 'First Post',
-        'content': 'first post content...'
-    },
-    {
-        'author': 'Spock',
-        'title': 'Second Post',
-        'content': 'second post content...'
-    } ] #will be passed into a template as context variable
-
+from django.http import HttpResponse
+from .models import * #imports Blogpost model class
 
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Blog Homepage</h1>')
-    return render(request, 'blog/home.html', {'posts': posts}) #render still returns an HTTP Response
+    return render(request, 'blog/home.html', {'posts': Blogpost.objects.all()}) #passing in QuerySet of Blogpost model objects (table rows)
 
 def about(request, date):
     return render(request, 'blog/about.html', {"today": f"{date.month}/{date.day}"})
