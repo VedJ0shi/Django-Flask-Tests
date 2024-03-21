703d6de270bcbb88185d8220aa8d0bf4237a91a4
Ved J0shi <vedjoshi19104@gmail.com>
2024-03-21 01:26:02 -0400

    created djangoORMtests

--- djangoormtests/djangoormtests/asgi.py
+++ djangoormtests/djangoormtests/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djmodeltests project.
+ASGI config for djangoormtests project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djmodeltests.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoormtests.settings')
 
 application = get_asgi_application()
